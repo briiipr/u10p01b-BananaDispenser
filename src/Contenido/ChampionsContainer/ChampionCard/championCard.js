77f50bbeb54d895bdb7d62d96caa98d205c3dea5
CristinaDarias <cmdarias@gmail.com>
2020-02-29 02:37:39 +0000

    Modificado el elemento ChampionCard y demás estilos

--- src/Contenido/ChampionsContainer/ChampionCard/championCard.js
+++ src/Contenido/ChampionsContainer/ChampionCard/championCard.js
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import './championCard.css';
 
 function ChampionCard(campeon, img) {
     /* console.log('VALOR CAMPEON RECIBIDO: ')
     console.log(campeon)
     console.log('VALOR IMG: ' + img) */
 
- /*    return (
-        console.log('SE ACTUALIZA ' + campeon),
-        <label>Campeon.</label>
-    );
- */
+    /*    return (
+           console.log('SE ACTUALIZA ' + campeon),
+           <label>Campeon.</label>
+       );
+       
+       <div className="card">
+               <img src={img}></img>
+               <p>{campeon.id}</p>
+           </div>
+    */
     return (
-        <div className="card">
-            <img src={img}></img>
-            <p>{campeon.id}</p>
-        </div>
+        <Card className="text-center" style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={img} />
+            <Card.Body>
+                <Card.Title>{campeon.id}</Card.Title>
+                <Button variant="primary">Saber más</Button>
+            </Card.Body>
+        </Card>
     );
 }
 
